95b214630f43772cf8e2bf574caa62e5bc8964fe
collins lenjo <collinslenjo.cl@gmail.com>
2017-08-02 21:31:16 +0300

    added libraries

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+  	[org.clojure/clojure "1.8.0"]
+  	[ring "1.0.1"]
+  	[compojure "1.0.1"]
+  	[enlive "1.0.0"]]
   :main hello.core)
