50cb722ae75232e29f35af0a6b26f5c3849677fd
collins lenjo <collinslenjo.cl@gmail.com>
2017-08-03 17:29:35 +0300

    home

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
   	[org.clojure/clojure "1.8.0"]
-  	[ring "1.0.1"]
-  	[compojure "1.0.1"]
-  	[enlive "1.0.0"]]
-  :main hello.app)
+  	[ring "1.6.2"]
+  	[compojure "1.6.0"]
+  	[enlive "1.1.6"]])
--- src/hello/app.clj
+++ src/hello/app.clj
@@ -1,7 +1,29 @@
 (ns hello.app
-	(:require [ring.adapter.jetty :as jetty]))
+  (:use [compojure handler
+         [core :only (GET POST defroutes)]])
+	(:require [net.cgrand.enlive-html :as en]
+            [ring.util.response :as response]
+            [ring.adapter.jetty :as jetty]))
 
-(defn app
-	[request]
-	{:status 200
-	 :body (:url request)})
+(defonce counter (atom 10000))
+(defonce urls (atom {}))
+
+(defn shorten
+  [url]
+  (let [id (swap! counter inc)
+        id (Long/toString id 36)]
+    (swap! urls assoc id url)
+    id))
+
+(en/deftemplate homepage
+  (en/xml-resource "homepage.html")
+  [request]
+  )
+
+(defn redirect
+   [id]
+   (response/redirect (@urls id)))
+
+(defroutes app
+  (GET "/" request (homepage request))
+  (GET "/:id" [id] (redirect request)))
