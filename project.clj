00333ecb88017fff8588c8a626a3f0025e79dfc7
collins lenjo <collinslenjo.cl@gmail.com>
2017-08-23 21:48:57 +0300

    changes dependancies

--- project.clj
+++ project.clj
@@ -6,5 +6,10 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
   				 [javax.servlet/servlet-api "2.5"]
   				 [ring "1.6.2"]
-                 [ring/ring-core "1.6.2"]
-                 [ring/ring-jetty-adapter "1.6.2"]])
+  				 [compojure "1.6.0"]
+  				 [hiccup "1.0.5"]])
+
+;[ring/ring-core "1.6.2"]
+;[ring/ring-jetty-adapter "1.6.2"]
+;[org.clojure/java.jdbc "0.6.0"]
+;[com.h2database/h2 "1.4.193"]
