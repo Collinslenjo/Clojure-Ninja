a0e5556d1c1b21b4aaa9a2174e5511b4a549aa5c
collins lenjo <collinslenjo.cl@gmail.com>
2017-08-25 20:31:46 +0300

    Trying to add automatic server start

--- src/ninja_app/core.clj
+++ src/ninja_app/core.clj
@@ -1,6 +1,10 @@
-(ns ninja-app.core)
+(ns ninja-app.core
+	(:use ring.adapter.jetty))
 
 (defn handler [request]
-  {:status 200
+  (Thread/sleep 100)
+  {:status  200
    :headers {"Content-Type" "text/html"}
-   :body "Hello Ninjas"})
+   :body    "Hello Ninjas"})
+
+(defn run [] (run-jetty handler {:port 8080}))
